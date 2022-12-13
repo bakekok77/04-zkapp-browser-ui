50735d3c54c4caef52bdf2ba7d803506908fb165
bakekok77 <hanifahdyah2608@gmail.com>
2022-12-13 04:42:32 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-2ec65241670e2b81.js
+++ _next/static/chunks/webpack-2ec65241670e2b81.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "cb2ad3612a76a168" + ".css";
+/******/ 			return "static/css/" + "d62d37c87743236d" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
